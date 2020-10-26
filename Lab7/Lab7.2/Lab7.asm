a5fd9f11af64ea7a7ea568525c5af05d03bfcaeb
unknown <asgasebastian@gmail.com>
2020-10-26 01:10:24 -0600

    Corrección de error en proyecto 1.

--- Lab7/Lab7.2/Lab7.asm
+++ Lab7/Lab7.2/Lab7.asm
@@ -1,36 +1,30 @@
 .386
-.model flat, stdcall
-option casemap:none 
-include \masm32\include\windows.inc				
-include \masm32\include\kernel32.inc			
-include \masm32\include\masm32.inc				
-include \masm32\include\masm32rt.inc
-.data
-.data? 
-	carne db 10 dup (?)
-	carrera db 100 dup (?)
-	nombre db 100 dup (?)
-.const 
-.code
-	main:
-		print chr$("Ingresar Nombre: ")
-		invoke StdIn, addr nombre, 100
-		print chr$(13,10)
-		print chr$("Ingresar carné: ")
-		invoke StdIn, addr carne, 10
-		print chr$(13,10)
-		print chr$("Ingresar carrera: ")
-		invoke StdIn, addr carrera, 100
-		print chr$(13,10)
-
-		print chr$("Hola ")
-		invoke StdOut, addr nombre
-		print chr$(" su carne es: ")
-		invoke StdOut, addr carne
-		print chr$(13,10)
-
-		print chr$("Bienvenido a la carrera de ")
-		invoke StdOut, addr carrera
-		;FINALIZAR
-		invoke ExitProcess, 0
-end main
+.MODEL flat, stdcall
+OPTION casemap:none
+INCLUDE \masm32\include\windows.inc
+INCLUDE \masm32\include\kernel32.inc
+INCLUDE \masm32\include\masm32.inc
+INCLUDE \masm32\include\masm32rt.inc
+.DATA?
+    InputName DB 50 DUP(?)
+    InputID DB 9 DUP(?)
+    InputCar DB 50 DUP(?)
+.CODE
+Program:
+    print chr$("Ingrese su nombre: ")
+	INVOKE StdIn, ADDR InputName, 50
+	print chr$("Ingrese su carne: ")
+	INVOKE StdIn, ADDR InputID, 9
+	print chr$("Ingrese su carrera: ")
+    INVOKE StdIn, ADDR InputCar, 50
+	print chr$(13, 10)
+	print chr$("Hola, ")
+	INVOKE StdOut, ADDR InputName
+	print chr$(", su carne es ")
+	INVOKE StdOut, ADDR InputID
+	print chr$(".", 13, 10)
+	print chr$("Bienvenido a la carrera de ")
+	INVOKE StdOut, ADDR InputCar
+	print chr$(".")
+    INVOKE ExitProcess, 0
+END Program
